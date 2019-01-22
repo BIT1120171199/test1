ba2b63aaab5c2ada620c61fc2c4e02dfc2de790d
BIT1120171199 <1160520321@qq.com>
2019-01-22 20:33:33 +0800

    homework for chapter4

---
+++ Chapter4Classwork-fill_in_the_blanks_github/app/src/main/java/com/bytedance/android/lesson/restapi/solution/newtork/IMiniDouyinService.java
@@ -0,0 +1,27 @@
+package com.bytedance.android.lesson.restapi.solution.newtork;
+
+import com.bytedance.android.lesson.restapi.solution.bean.FeedResponse;
+import com.bytedance.android.lesson.restapi.solution.bean.PostVideoResponse;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.17 20:38
+ */
+public interface IMiniDouyinService {
+    // TODO-C2 (7) Implement your MiniDouyin PostVideo Request here, url: (POST) http://10.108.10.39:8080/minidouyin/video
+    // TODO-C2 (8) Implement your MiniDouyin Feed Request here, url: http://10.108.10.39:8080/minidouyin/feed
+    public static final String HOST = "http://10.108.10.39:8080/";
+    @POST("/minidouyin/video")
+    @Multipart
+    Call<PostVideoResponse> createVideo(@Query("student_id") String str, @Query("user_name") String str2, @Part MultipartBody.Part part, @Part MultipartBody.Part part2);
+    @GET("/minidouyin/feed")
+    Call<FeedResponse> fetchFeed();
+}
